f4076ff659b955759ce2eb274d28136178c3809d
TomiTurner10 <95268182+TomiTurner10@users.noreply.github.com>
2021-11-30 13:27:30 -0800

    Add files via upload

--- FileHandler.py
+++ FileHandler.py
@@ -4,6 +4,7 @@
 import tkinter as tk
 from tkinter import filedialog
 from Main import process
+from Main import open_html
 
 filename = "INF151_Syllabus.docx"
 
@@ -19,14 +20,23 @@ def processCallback():
     global filename
     process(filename)
 
+# Opens pdf files but downloads doc files
+def open_file():
+    global filename
+    open_html(filename)
+
 
 root = tk.Tk()
+root.title('Canvas Content Automation Tool')
+root.geometry("400x200")
 
 button = tk.Button(root, text='Choose File', command=SelectFileAction)
 button2 = tk.Button(root, text='Process', command=processCallback)
+button3 = tk.Button(root, text='Open File', command=open_file)
 
 button.pack()
 button2.pack()
+button3.pack()
 
 text = tk.StringVar()
 updateDisplay("No file chosen")
